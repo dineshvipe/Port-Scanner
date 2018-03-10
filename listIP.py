db12ad10c4929d46372c89ca82120cca0073eaaf
Dinesh Yadav <27940059+dineshvipe@users.noreply.github.com>
2018-03-10 23:54:32 +0530

    Listing IPs

--- listIP.py
+++ listIP.py
@@ -4,21 +4,35 @@ from socket import *
 import checkopenports
 from threading import Thread
 
-nm=nmap.PortScanner()
+nm=nmap.PortScanner()  #creates object
+
+#scanning the subnet for connected IPs
 data=nm.scan(hosts="172.16.163.*",arguments="-sP")
+
 print("Number of connected IPs: {} ".format(data['nmap']['scanstats']['uphosts']))
-#print(data['scan'])
+
+#storing List of IPs
 a=[ip for ip in data['scan']]
-#print(a)
+
 print("\n\tList of IPs Connected :")
+
 print("------------------------------------------------------")
+
 for i in a:
+    
     print("\t{}IP connected: {}{}".format("|",i,"|"))
-print("-------------------------------------------------------")
+
+ print("-------------------------------------------------------")
+
+#Scanning Ports
 for i in a:
-    #print("\tIP: {}".format(i))
+    
     print("Scanning {}".format(i))
+    
     port=[80,21,29,8090,25,443,445,14147,3306]
+    
     for j in port:
+        #creating threads
         t=Thread(target=checkopenports.checkPort,args=(i,str(j)))
+        
         t.start()
